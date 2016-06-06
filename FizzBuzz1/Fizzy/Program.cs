318d3bc7afd985a0293f3ab44cda2fc57d33e5e9
Jerry Key <netspec1@hotmail.com>
2016-06-06 00:29:46 -0400

    adding the completed fizzbuzz solution

---
+++ FizzBuzz1/Fizzy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int tot = 0; tot < 100; tot++)
+               // Console.WriteLine([tot]);
+            if (tot % 3 ==0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (tot % 5 == 0)
+                    {
+                    Console.WriteLine("Buzz");
+                }
+                else if (tot % 5 == 0 && tot % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(tot);
+                }
+            Console.ReadLine();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    
+     }
+}
